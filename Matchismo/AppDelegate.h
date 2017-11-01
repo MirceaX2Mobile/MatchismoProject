92ea58345a67d4462544a2fbd6d56922e456ee06
Dragota Mircea <dragotamircea@Dragotas-iMac.local>
2017-11-01 10:19:25 -0700

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Dragota Mircea on 01/11/2017.
+//  Copyright © 2017 Dragota Mircea. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
